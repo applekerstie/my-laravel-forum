293952ab1ae926adb2d56f70c088a5cf1ae1e379
applekerstie <ubuntu@DESKTOP-QRPDHRI>
2024-06-13 23:04:03 +0900

    Start Project

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    {{--<link rel="stylesheet" href="{{ mix('css/app.css') }}">--}}
+    <link rel="stylesheet" href="css/app.css" />
+    @yield('style')
+
+    <!-- Scripts -->
+  <script>
+    window.Laravel = <?php echo json_encode([
+      'csrfToken' => csrf_token(),
+      'currentUser' => $currentUser,
+      //'currentRouteName' => $currentRouteName,
+      //'currentLocale' => $currentLocale,
+      //'currentUrl' => $currentUrl,
+    ]); ?>
+  </script>
+</head>
+<body id="app-layout">
+    @include('layouts.partial.navigation')
+
+    <div class="container">
+        @include('flash::message')
+        @yield('content')
+    </div>
+
+    @include('layouts.partial.footer')
+
+    {{--<script src="{{ mix('js/app.js') }}"></script>--}}
+    <script src="js/app.js"></script>
+
+    @yield('script')
+</body>
+</html>
